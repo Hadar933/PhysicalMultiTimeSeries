754493e5147e2c9af097d6047b9fc02b03f86948
Hadar Sharvit <hadar.sharvit1@mail.huji.ac.il>
2023-04-12 18:41:13 +0300

    several improvements
    [main] added main.py, to run an entire experiment
    [trainer] changes to the return type of .predict, working on trainer yaml file
    [utils] yaml writer func, and some minor updates

--- main.py
+++ main.py
@@ -1,33 +1,74 @@
+from typing import Optional, List
+
+import pandas as pd
+
 from trainer import Trainer
 import matplotlib.pyplot as plt
-from utils import load_data_from_prssm_paper, train_val_test_split
+import utils
 import torch
 import torch.nn as nn
 from torchsummary import summary
 
 
-def main():
-	kinematics, forces = load_data_from_prssm_paper()
-	feature_lag, target_lag, intersect = 120, 1, 0
-	train_percent, val_percent = 0.9, 0.09
-	batch_size = 64
-	n_features = kinematics.shape[-1]
-	# input: (64,120,3)
-	model = nn.Sequential()
-	print(summary(model, (1, feature_lag, n_features)))
-	optimizer = torch.optim.Adam(model.parameters())
-	trainer = Trainer(model, 'linear', optimizer)
-	train_loader, val_loader, all_test_dataloaders = train_val_test_split(kinematics, forces, train_percent, val_percent,
-		feature_lag, target_lag, intersect, batch_size)
-	trainer.fit(train_loader, val_loader)
-	for i in range(len(all_test_dataloaders)):
-		preds, trues = trainer.predict(all_test_dataloaders[i])
-		plt.plot(preds, label="Predictions")
-		plt.plot(trues, label="True Labels")
-		plt.legend()
-		plt.title(f"Test set #{i}, MAE={nn.functional.l1_loss(preds, trues)}")
-		plt.show()
+def main(features: torch.Tensor,
+         targets: torch.Tensor,
+         model: nn.Module,
+         model_name: str,
+         feature_lag: Optional[int] = 120,
+         target_lag: Optional[int] = 1,
+         intersect: Optional[int] = 0,
+         train_percent: Optional[float] = 0.9,
+         val_percent: Optional[float] = 0.09,
+         batch_size: Optional[int] = 64,
+         criterion: Optional[torch.nn.modules.loss._Loss] = torch.nn.L1Loss(),
+         device: Optional[torch.cuda.device] = torch.device("cuda:0" if torch.cuda.is_available() else "cpu"),
+         patience_tolerance: Optional[float] = 0.005,
+         n_epochs: Optional[int] = 100,
+         patience: Optional[int] = 10,
+         seed: Optional[int] = 3407):
+    n_features = features.shape[-1]
+    # print(summary(model, (1, feature_lag, n_features)))
+    optimizer = torch.optim.Adam(model.parameters())
+    train_loader, val_loader, test_loaders = utils.train_val_test_split(features, targets, train_percent, val_percent,
+                                                                        feature_lag, target_lag, intersect, batch_size)
+    trainer = Trainer(train_loader, val_loader, test_loaders, model, model_name, optimizer, criterion, device,
+                      patience, patience_tolerance, n_epochs, seed)
+    trainer.load_trained_model(
+        "G:\My Drive\Master\Lab\Experiment\ForcePrediction\Models\lstm_2023-04-12_17-08-28/best_lstm_2023-04-12_17-08-28_epoch_0.pt")
+    # trainer.fit()
+    predictions = trainer.predict()
+    return {'trainer': trainer,
+            'predictions': predictions}
 
 
 if __name__ == '__main__':
-	main()
+    # 64 x 120 x 7
+    mlp_model = nn.Sequential(
+        nn.Flatten(),
+        nn.Linear(840, 16),
+        nn.ReLU(),
+        nn.Linear(16, 8),
+        nn.ReLU(),
+        nn.Linear(8, 1)
+    )
+
+
+    class LSTM(nn.Module):
+        def __init__(self):
+            super(LSTM, self).__init__()
+            self.lstm = nn.LSTM(7, 32, 1, batch_first=True)
+            self.fc = nn.Linear(32, 1)
+
+        def forward(self, x):
+            x, _ = self.lstm(x)
+            x = x[:, -1, :]
+            x = self.fc(x)
+            return x
+
+
+    lstm_model = LSTM()
+    mod_name = "lstm"
+    kinematics, forces = utils.load_data_from_prssm_paper(kinematics_key='ds_u')
+
+    ret = main(kinematics, forces, lstm_model, mod_name, n_epochs=1)
+    x = 2
--- trainer.py
+++ trainer.py
@@ -1,7 +1,8 @@
 from datetime import datetime
-from typing import Optional
-
+from typing import Optional, List, Tuple, Dict
+import utils
 import pandas as pd
+import yaml
 from torch.utils.tensorboard import SummaryWriter
 import psutil
 import torch
@@ -15,125 +16,141 @@ TIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
 
 
 class Trainer:
-	"""
-	a class that takes in a model and some training parameters and performs testing, evaluating and prediction
-	"""
-	
-	def __init__(self,
-				 model: torch.nn.Module,
-				 model_name: str,
-				 optimizer: torch.optim.Optimizer,
-				 criterion: Optional[torch.nn.modules.loss._Loss] = torch.nn.MSELoss(),
-				 device: Optional[torch.cuda.device] = torch.device("cuda:0" if torch.cuda.is_available() else "cpu"),
-				 patience_tolerance: float = 0.005,
-				 n_epochs: Optional[int] = 100,
-				 patience: Optional[int] = 10,
-				 seed: int = 3407):
-		torch.manual_seed(seed)
-		self.patience_tolerance: float = patience_tolerance
-		self.early_stopping: int = 0
-		self.model_name: str = model_name
-		self.criterion: torch.nn.modules.loss = criterion
-		self.optimizer: torch.optim.Optimizer = optimizer
-		self.device: torch.cuda.device = device
-		self.model: torch.nn.Module = model.to(device)
-		self.n_epochs: int = n_epochs
-		self.patience: int = patience
-		self.stop_training: bool = False
-		self.tb_writer: SummaryWriter = None
-		self.best_val_loss: float = float('inf')
-		self.model_dir: str = ""
-		self.best_model_path: str = ""
-		self._create_model_dir()
-	
-	def _create_model_dir(self):
-		""" called when a trainer is initialized and creates a model dir with model architecture .txt file """
-		init_timestamp = datetime.now().strftime(TIME_FORMAT)
-		self.model_dir = f"{os.getcwd()}\\Models\\{self.model_name}_{init_timestamp}"
-		if not os.path.exists(self.model_dir):
-			os.makedirs(self.model_dir)
-		with open(f"{self.model_dir}/architecture", 'w') as f:
-			f.write(str(self.model))
-	
-	def _train_one_epoch(self, epoch: int, train_loader: torch.utils.data.DataLoader) -> float:
-		"""
-		performs a training process for one epoch
-		:param epoch: the current epoch number
-		:param train_loader: the training data loader
-		"""
-		self.model.train(True)
-		total_loss = 0.0
-		tqdm_loader = tqdm(train_loader)
-		for inputs, targets in tqdm_loader:
-			self.optimizer.zero_grad()
-			inputs, targets = inputs.to(self.device), targets.to(self.device)
-			predictions = self.model(inputs)
-			loss = self.criterion(predictions, targets)
-			total_loss += loss
-			loss.backward()
-			self.optimizer.step()
-			tqdm_loader.set_postfix({'Train Loss': f"{loss.item():.5f}",
-									 'RAM_%': psutil.virtual_memory().percent,
-									 'Epoch': epoch})
-		total_loss /= len(train_loader)
-		return total_loss
-	
-	def _evaluate(self, epoch: int, val_loader: torch.utils.data.DataLoader) -> float:
-		self.model.train(False)
-		total_loss = 0.0
-		with torch.no_grad():
-			tqdm_loader = tqdm(val_loader)
-			for inputs, targets in tqdm_loader:
-				inputs, targets = inputs.to(self.device), targets.to(self.device)
-				predictions = self.model(inputs)
-				loss = self.criterion(predictions, targets)
-				total_loss += loss
-				tqdm_loader.set_postfix({'Val Loss': f"{loss.item():.5f}",
-										 'RAM_%': psutil.virtual_memory().percent,
-										 'Epoch': epoch})
-			total_loss /= len(val_loader)
-		return total_loss
-	
-	def _finish_epoch(self, val_loss, prev_val_loss, time, epoch):
-		if val_loss < self.best_val_loss:  # if val is improved, we update the best model
-			self.best_val_loss = val_loss
-			if self.best_model_path:
-				os.remove(self.best_model_path)
-			self.best_model_path = f"{self.model_dir}/best_{self.model_name}_{time}_epoch_{epoch}.pt"
-			torch.save(self.model.state_dict(), self.best_model_path)
-		elif torch.abs(val_loss - prev_val_loss) <= self.patience_tolerance:  # if val doesn't improve, counter += 1
-			self.early_stopping += 1
-		if self.early_stopping >= self.patience:  # if patience value is reached, the training process halts
-			self.stop_training = True
-			print(f"[Early Stopping] at epoch {epoch}.")
-	
-	def fit(self, train_loader: torch.utils.data.DataLoader, val_loader: torch.utils.data.DataLoader) -> None:
-		fit_time = datetime.now().strftime(TIME_FORMAT)
-		self.tb_writer = SummaryWriter()
-		prev_val_loss = float('inf')
-		for epoch in range(self.n_epochs):
-			train_avg_loss = self._train_one_epoch(epoch, train_loader)
-			val_avg_loss = self._evaluate(epoch, val_loader)
-			self.tb_writer.add_scalar('Loss/Train', train_avg_loss, epoch)
-			self.tb_writer.add_scalar('Loss/Val', val_avg_loss, epoch)
-			self._finish_epoch(val_avg_loss, prev_val_loss, fit_time, epoch)
-			if self.stop_training:
-				break
-			prev_val_loss = val_avg_loss
-	
-	def predict(self, test_loader: torch.utils.data.DataLoader):
-		self.model.train(False)
-		predictions, true_labels = [], []
-		with torch.no_grad():
-			for inputs, true in test_loader:
-				inputs.to(self.device)
-				pred = self.model(inputs)
-				predictions.append(pred.item())
-				true_labels.append(true.item())
-		return torch.Tensor(predictions), torch.Tensor(true_labels)
-	
-	def load_trained_model(self, trained_model_path: str):
-		self.model.load_state_dict(torch.load(trained_model_path))
-		self.model.eval()
+    """
+    a class that takes in a model and some training parameters and performs testing, evaluating and prediction
+    """
 
+    def __init__(self,
+                 train_loader: torch.utils.data.DataLoader,
+                 val_loader: torch.utils.data.DataLoader,
+                 all_test_loaders: List[torch.utils.data.DataLoader],
+                 model: torch.nn.Module,
+                 model_name: str,
+                 optimizer: torch.optim.Optimizer,
+                 criterion: torch.nn.modules.loss._Loss,
+                 device: torch.cuda.device,
+                 patience: int,
+                 patience_tolerance: float,
+                 n_epochs: int,
+                 seed: int):
+        torch.manual_seed(seed)
+        self.train_loader = train_loader
+        self.val_loader = val_loader
+        self.all_test_loaders = all_test_loaders
+        self.patience_tolerance: float = patience_tolerance
+        self.early_stopping: int = 0
+        self.model_name: str = model_name
+        self.criterion: torch.nn.modules.loss = criterion
+        self.optimizer: torch.optim.Optimizer = optimizer
+        self.device: torch.cuda.device = device
+        self.model: torch.nn.Module = model.to(device)
+        self.n_epochs: int = n_epochs
+        self.patience: int = patience
+        self.stop_training: bool = False
+        self.tb_writer: SummaryWriter = None
+        self.best_val_loss: float = float('inf')
+        self.model_dir: str = ""
+        self.best_model_path: str = ""
+        self.yaml_path = ""
+        self._create_model_dir()
 
+    def _create_model_dir(self):
+        """ called when a trainer is initialized and creates a model dir with relevant information txt file(s) """
+        init_timestamp = datetime.now().strftime(TIME_FORMAT)
+        self.model_dir = f"{os.getcwd()}\\Models\\{self.model_name}_{init_timestamp}"
+        self.yaml_path = f"{self.model_dir}\\trainer_info.yaml"
+        if not os.path.exists(self.model_dir):
+            os.makedirs(self.model_dir)
+        with open(self.yaml_path, 'w') as f: yaml.dump({}, f)  # creating an empty args.yaml
+        utils.update_yaml(self.yaml_path, {'model': str(self.model), 'patience': self.patience,
+                                           'patience_tolerance': self.patience_tolerance, 'loss': str(self.criterion),
+                                           'optim': str(self.optimizer), 'epochs': self.n_epochs})
+
+    def _train_one_epoch(self, epoch: int) -> float:
+        """ performs a training process for one epoch """
+        self.model.train(True)
+        total_loss = 0.0
+        tqdm_loader = tqdm(self.train_loader)
+        for inputs, targets in tqdm_loader:
+            self.optimizer.zero_grad()
+            inputs, targets = inputs.to(self.device), targets.to(self.device)
+            predictions = self.model(inputs)
+            loss = self.criterion(predictions, targets)
+            total_loss += loss
+            loss.backward()
+            self.optimizer.step()
+            tqdm_loader.set_postfix({'Train Loss': f"{loss.item():.5f}",
+                                     'RAM_%': psutil.virtual_memory().percent,
+                                     'Epoch': epoch})
+        total_loss /= len(self.train_loader)
+        return total_loss
+
+    def _evaluate(self, epoch: int) -> float:
+        """ evaluates the model on the validation set """
+        self.model.train(False)
+        total_loss = 0.0
+        with torch.no_grad():
+            tqdm_loader = tqdm(self.val_loader)
+            for inputs, targets in tqdm_loader:
+                inputs, targets = inputs.to(self.device), targets.to(self.device)
+                predictions = self.model(inputs)
+                loss = self.criterion(predictions, targets)
+                total_loss += loss
+                tqdm_loader.set_postfix({'Val Loss': f"{loss.item():.5f}",
+                                         'RAM_%': psutil.virtual_memory().percent,
+                                         'Epoch': epoch})
+            total_loss /= len(self.val_loader)
+        return total_loss
+
+    def _finish_epoch(self, val_loss, prev_val_loss, time, epoch):
+        """ saves the best model and performs early stopping if needed """
+        if val_loss < self.best_val_loss:  # if val is improved, we update the best model
+            self.best_val_loss = val_loss
+            if self.best_model_path:
+                os.remove(self.best_model_path)
+            self.best_model_path = f"{self.model_dir}/best_{self.model_name}_{time}_epoch_{epoch}.pt"
+            torch.save(self.model.state_dict(), self.best_model_path)
+        elif torch.abs(val_loss - prev_val_loss) <= self.patience_tolerance:  # if val doesn't improve, counter += 1
+            self.early_stopping += 1
+        if self.early_stopping >= self.patience:  # if patience value is reached, the training process halts
+            self.stop_training = True
+            print(f"[Early Stopping] at epoch {epoch}.")
+
+    def fit(self) -> None:
+        """ fits the model to the training data, with early stopping """
+        fit_time = datetime.now().strftime(TIME_FORMAT)
+        self.tb_writer = SummaryWriter(f'runs/{self.model_name}_{fit_time}')
+        prev_val_loss = float('inf')
+        for epoch in range(self.n_epochs):
+            train_avg_loss = self._train_one_epoch(epoch)
+            val_avg_loss = self._evaluate(epoch)
+            self.tb_writer.add_scalar('Loss/Train', train_avg_loss, epoch)
+            self.tb_writer.add_scalar('Loss/Val', val_avg_loss, epoch)
+            self._finish_epoch(val_avg_loss, prev_val_loss, fit_time, epoch)
+            if self.stop_training:
+                break
+            prev_val_loss = val_avg_loss
+        utils.update_yaml(self.yaml_path, {"early_stopping": self.early_stopping,
+                                           "best_val_loss": self.best_val_loss.value(),
+                                           "best_model_path": self.best_model_path})
+
+    def predict(self) -> pd.DataFrame:
+        """ creates a prediction for every test loader in our test loaders list """
+        self.model.train(False)
+        all_preds = pd.DataFrame()
+        with torch.no_grad():
+            for j, test_loader in enumerate(self.all_test_loaders):
+                curr_preds, curr_trues = [], []
+                for inputs_i, true_i in test_loader:
+                    inputs_i.to(self.device)
+                    pred_i = self.model(inputs_i)
+                    curr_preds.append(pred_i.item())
+                    curr_trues.append(true_i.item())
+                all_preds[f"pred_{j}"] = curr_preds
+                all_preds[f"true_{j}"] = curr_trues
+        return all_preds
+
+    def load_trained_model(self, trained_model_path: str) -> None:
+        """ loads into the trainer a trained model from memory """
+        self.model.load_state_dict(torch.load(trained_model_path))
+        self.model.eval()
