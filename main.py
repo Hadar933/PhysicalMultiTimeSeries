1175e30633a000fb12c8d35a144c8ce7def4dca7
Hadar Sharvit <hadar.sharvit1@mail.huji.ac.il>
2023-04-29 10:35:22 +0300

    minor changes to main example

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     input_dim, output_dim = forces.shape[-1], kinematics.shape[-1]
     hidden_dim, num_lstms = 32, 1
     model = zoo.LSTM(input_dim, hidden_dim, num_lstms, output_dim)
-    model_name = "lstm_fc_minmax_input"
+    model_name = "lstm_fc_minmax_input_and_targets"
     train_percent, val_percent = 0.9, 0.09
     feature_win, target_win, intersect = 120, 1, 0
     batch_size = 64
@@ -25,13 +25,13 @@ if __name__ == '__main__':
     patience, patience_tolerance = 10, 0.005
     n_epochs = 20
     seed = 3407
-    # model = zoo.MLP(forces.shape[-1], feature_win, kinematics.shape[-1], [10, 20, 30])
     optimizer = torch.optim.Adam(model.parameters())
     features_norm = 'minmax'
     targets_norm = 'minmax'
     trainer = Trainer(forces, kinematics, train_percent, val_percent, feature_win, target_win, intersect, batch_size,
                       model, model_name, optimizer, criterion, device, patience, patience_tolerance, n_epochs, seed,
                       features_norm, targets_norm)
+    # trainer.load_trained_model('G:\My Drive\Master\Lab\Experiment\MultiTimeSeries\Models\lstm_fc_normalized_input_2023-04-28_12-39-41\\best_lstm_fc_normalized_input_2023-04-28_12-39-41_epoch_14.pt')
     trainer.fit()
     ret = trainer.predict()
     ret = utils.format_df_torch_entries(ret)
