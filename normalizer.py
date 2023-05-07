ac8fd10f19f1daa86d17b615ca9e9e7f0c8aaade
Hadar Sharvit <hadar.sharvit1@mail.huji.ac.il>
2023-05-07 12:52:09 +0300

    Update normalizer.py
    added normalizer factory, and global normalilzation support

--- normalizer.py
+++ normalizer.py
@@ -1,4 +1,6 @@
-from typing import Dict, Optional
+import random
+from abc import ABC, abstractmethod
+from typing import Dict, Optional, Literal, Union
 
 import pandas as pd
 import torch
@@ -6,87 +8,164 @@ import torch
 from MultiTimeSeries import utils
 
 
-class Normalizer:
-    def __init__(self, method: str = 'zscore', history_dim: int = 1):
-        self.method: str = method
-        self.history_dim: int = history_dim
-        self.norm_params: Dict = dict()  # like min, max or mean, std
-
-    def fit_transform(self, tensor: torch.Tensor) -> torch.Tensor:
+class Normalizer(ABC):
+    def __init__(self, global_normalizer: bool = True):
         """
-        normalizes (feature-wise) a tensor with shape (N,H,F)
+        initializes a normalizer object.
+        :param global_normalizer: boolean value that governs the normalization dimension of an input
+                                  tensor with shape (N,H,F):
+                                    - If True: casts to (N*H,F) and normalizes over dim=0
+                                    - If False: keeps as (N,H,F) and normalizes over dim=1
+        """
+        self.global_normalizer = global_normalizer
+        self.norm_dim = 0 if self.global_normalizer else 1  # for dimensions (N*H,F) or (N,H,F)
+
+    def _handle_shape(self, tensor: torch.Tensor) -> torch.Tensor:
+        """ casts an input tensor to the required dimension given the global norm boolean"""
+        n_datasets, history_size, input_size = tensor.shape
+        return tensor.reshape(n_datasets * history_size, input_size) if self.global_normalizer else tensor
+
+    @abstractmethod
+    def fit(self, tensor: torch.Tensor) -> None:
+        """ computes relevant statistics to be used for later scaling. """
+        raise NotImplementedError
+
+    @abstractmethod
+    def transform(self, tensor: torch.Tensor) -> torch.Tensor:
+        """
+        normalizes (feature-wise) a tensor with shape (N,H,F). assumes the statistics are already given
         :return: tensor t such that for every i=0,1,2,...,N-1, the matrix t[i,:,:] columns are normalized
         """
-        if self.method == 'zscore':
-            return self._zscore(tensor)
-        elif self.method == 'minmax':
-            return self._minmax(tensor)
-        elif self.method == 'identity':
-            return tensor
-        else:
-            raise ValueError(f"Normalization method '{self.method}' not recognized.")
+        raise NotImplementedError
 
-    def inverse_transform(self, tensor: torch.Tensor, test_loader_idx: Optional[int] = None) -> torch.Tensor:
-        """ converts a normalized tensor to its original values. only works after fit_transform was used """
-        assert len(self.norm_params) > 0, 'Uninitialized norm params. Use fit_transform first'
-        if self.method == 'zscore':
-            return self._inv_zscore(tensor, test_loader_idx)
-        elif self.method == 'minmax':
-            return self._inv_minmax(tensor, test_loader_idx)
-        elif self.method == 'identity':
-            return tensor
-        else:
-            raise ValueError(f"Normalization method '{self.method}' not recognized.")
+    @abstractmethod
+    def fit_transform(self, tensor: torch.Tensor) -> torch.Tensor:
+        """ calls fit and then transform one after the other """
+        raise NotImplementedError
 
-    def _zscore(self, tensor: torch.Tensor) -> torch.Tensor:
-        """ performs zscore normalization on the tensor """
-        mean_val = tensor.mean(self.history_dim, keepdim=True)
-        std_val = tensor.std(self.history_dim, keepdim=True)
-        self.norm_params = {'mean': mean_val, 'std': std_val}
-        norm_tensor = (tensor - mean_val) / std_val
-        return norm_tensor
+    @abstractmethod
+    def inverse_transform(self, tensor: torch.Tensor) -> torch.Tensor:
+        """ converts a normalized tensor to its original values. assumes the statistics are already given. """
+        raise NotImplementedError
 
-    def _minmax(self, tensor: torch.Tensor) -> torch.Tensor:
-        """ performs minmax normalization on the tensor """
-        min_val = tensor.min(self.history_dim, keepdim=True).values
-        max_val = tensor.max(self.history_dim, keepdim=True).values
-        self.norm_params = {'min': min_val, 'max': max_val}
-        norm_tensor = (tensor - min_val) / (max_val - min_val)
-        return norm_tensor
 
-    def _inv_zscore(self, tensor: torch.Tensor, test_loader_idx: Optional[int] = None) -> torch.Tensor:
-        mean_val = self.norm_params['mean']
-        std_val = self.norm_params['std']
-        if test_loader_idx is not None:
-            mean_val = mean_val[test_loader_idx].unsqueeze(0)
-            std_val = std_val[test_loader_idx].unsqueeze(0)
-        inv_norm_tensor = tensor * std_val + mean_val
+class ZScore(Normalizer):
+    """ zscore scaler, maps x -> [ x - mean(x) ] / [ std(x) ]"""
+
+    def __init__(self, global_normalizer: bool = True):
+        super().__init__(global_normalizer)
+        self.mean_val = None
+        self.std_val = None
+
+    def fit(self, tensor: torch.Tensor) -> torch.Tensor:
+        tensor = self._handle_shape(tensor)
+        self.mean_val = tensor.mean(self.norm_dim, keepdim=True)
+        self.std_val = tensor.std(self.norm_dim, keepdim=True)
+
+    def transform(self, tensor: torch.Tensor) -> torch.Tensor:
+        zscore_tensor = (tensor - self.mean_val) / self.std_val
+        return zscore_tensor
+
+    def fit_transform(self, tensor: torch.Tensor) -> torch.Tensor:
+        self.fit(tensor)
+        return self.transform(tensor)
+
+    def inverse_transform(self, tensor: torch.Tensor) -> torch.Tensor:
+        inv_norm_tensor = tensor * self.std_val + self.mean_val
         return inv_norm_tensor
 
-    def _inv_minmax(self, tensor: torch.Tensor, test_loader_idx: Optional[int] = None) -> torch.Tensor:
-        min_val = self.norm_params['min']
-        max_val = self.norm_params['max']
-        if test_loader_idx is not None:
-            min_val = min_val[test_loader_idx].unsqueeze(0)
-            max_val = max_val[test_loader_idx].unsqueeze(0)
-        inv_minmax_tensor = tensor * (max_val - min_val) + min_val
+
+class MinMax(Normalizer):
+    """ min-max scaler, maps x -> [ x - min(x) ] / [ max(x) - min(x) ] """
+
+    def __init__(self, global_normalizer: bool = True):
+        super().__init__(global_normalizer)
+        self.min_val = None
+        self.max_val = None
+
+    def fit(self, tensor: torch.Tensor) -> torch.Tensor:
+        tensor = self._handle_shape(tensor)
+        self.min_val = tensor.min(self.norm_dim, keepdim=True).values
+        self.max_val = tensor.max(self.norm_dim, keepdim=True).values
+
+    def transform(self, tensor: torch.Tensor) -> torch.Tensor:
+        minmax_tensor = (tensor - self.min_val) / (self.max_val - self.min_val)
+        return minmax_tensor
+
+    def fit_transform(self, tensor: torch.Tensor) -> torch.Tensor:
+        self.fit(tensor)
+        return self.transform(tensor)
+
+    def inverse_transform(self, tensor: torch.Tensor) -> torch.Tensor:
+        inv_minmax_tensor = tensor * (self.max_val - self.min_val) + self.min_val
         return inv_minmax_tensor
 
 
-if __name__ == '__main__':
-    from sklearn.preprocessing import StandardScaler
+class Identity(Normalizer):
+    """ An identity 'normalizer' to be used when no normalization is wanted """
 
-    scaler = StandardScaler()  # for testing purposes
+    def __init__(self, global_normalizer: bool = True):
+        super().__init__(global_normalizer)
+
+    def fit(self, tensor: torch.Tensor) -> None:
+        return
+
+    def transform(self, tensor: torch.Tensor) -> torch.Tensor:
+        return tensor
+
+    def fit_transform(self, tensor: torch.Tensor) -> torch.Tensor:
+        return tensor
+
+    def inverse_transform(self, tensor: torch.Tensor) -> torch.Tensor:
+        return tensor
+
+
+class NormalizerFactory:
+    """ a normalizers factory class """
+
+    @staticmethod
+    def create(normalizer_type: Union[Literal['zscore', 'minmax', 'identity'], None],
+               global_normalizer: bool = True):
+        if normalizer_type == 'zscore':
+            return ZScore(global_normalizer)
+        elif normalizer_type == 'minmax':
+            return MinMax(global_normalizer)
+        elif normalizer_type == 'identity' or normalizer_type is None:
+            return Identity(global_normalizer)
+        else:
+            raise ValueError(f"Invalid normalizer type: {normalizer_type}")
+
+
+if __name__ == '__main__':
+    from sklearn.preprocessing import StandardScaler, MinMaxScaler
+
+    zscaler = StandardScaler()
+    mmscaler = MinMaxScaler()
     kinematics, forces = utils.load_data_from_prssm_paper()
-    N = Normalizer('zscore')
-    n_forces = N.fit_transform(forces)
-    inv_forces = N.inverse_transform(n_forces)
-    exp = 0
-    f, fn, finv = forces[exp, :, :], n_forces[exp, :, :], inv_forces[exp, :, :]
-    fsk = torch.from_numpy(scaler.fit_transform(f.numpy()))
-    df_f = pd.DataFrame(f, columns=[f"f_{i}" for i in range(f.shape[-1])])
-    df_fn = pd.DataFrame(fn, columns=[f"fn_{i}" for i in range(fn.shape[-1])])
-    df_fsk = pd.DataFrame(fsk, columns=[f"fsk_{i}" for i in range(fsk.shape[-1])])
-    df_finv = pd.DataFrame(finv, columns=[f"finv_{i}" for i in range(finv.shape[-1])])
-    df = pd.concat([df_f, df_fn, df_fsk, df_finv], axis=1)
-    utils.plot(df)
+    N, H, F = forces.shape
+    exp = random.randint(0, N - 1)
+    for global_normalizer in [False]:
+        Z = NormalizerFactory.create('zscore', global_normalizer)
+        MM = NormalizerFactory.create('minmax', global_normalizer)
+
+        z_forces = Z.fit_transform(forces)
+        invz_forces = Z.inverse_transform(z_forces)
+
+        mm_forces = MM.fit_transform(forces)
+        invmm_forces = MM.inverse_transform(mm_forces)
+
+        # sk_forces = forces.reshape(N * H, F).numpy() if global_normalizer else forces.numpy()
+
+        # z_sk_forces = torch.from_numpy(zscaler.fit_transform(sk_forces)).reshape(N, H, F)
+        # mm_sk_forces = torch.from_numpy(mmscaler.fit_transform(sk_forces)).reshape(N, H, F)
+
+        df_f = pd.DataFrame(forces[exp], columns=[f'Original_{i}' for i in range(F)])
+        # df_fskz = pd.DataFrame(z_sk_forces[exp], columns=[f"Sklearn_Zcore_{i}" for i in range(F)])
+        # df_fskmm = pd.DataFrame(mm_sk_forces[exp], columns=[f"Sklearn_MinMax_{i}" for i in range(F)])
+        df_fz = pd.DataFrame(z_forces[exp], columns=[f"My_Zcore_{i}" for i in range(F)])
+        df_finvz = pd.DataFrame(invz_forces[exp], columns=[f"My_Inv_Zscore_{i}" for i in range(F)])
+        df_fmm = pd.DataFrame(mm_forces[exp], columns=[f"My_MinMax_{i}" for i in range(F)])
+        df_finvmm = pd.DataFrame(invmm_forces[exp], columns=[f"My_Inv_MinMax_{i}" for i in range(F)])
+
+        df = pd.concat([df_f, df_fz, df_fmm, df_finvz, df_finvmm],axis=1)#, df_fskz, df_fskmm], axis=1)
+        # utils.plot(df, title=f"global_norm={global_normalizer}, experiment#={exp}")
